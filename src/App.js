d6baad3ac0f7a050b329aa6fa1428676bb94c464
vinicius viana <viniciuspro333@gmail.com>
2023-11-12 14:47:01 -0300

    Dev House Front-End

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Heart, Menu, Phone, Search, User2 } from "lucide-react";
+import { BrowserRouter } from "react-router-dom";
+
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <header className="bg-white py-5 px-3 w-full top-0 flex flex-row items-center justify-between">
+          <div className="flex items-center bg-slate-100 px-3 py-2 w-full max-w-xs lg:max-w-[50%] rounded-lg lg:mr-8">
+            <Search />
+            <input type="text" className="w-full bg-slate-100 px-2 outline-none" placeholder="Pesquise aqui" />
+          </div>
+          <nav className="mr-auto hidden md:flex">
+            <a href="#" className="text-gray-500 font-medium hover:text-black hover:bg-gray-100 p-2 rounded-lg duration-300">
+              Aluguel
+            </a>
+            <a href="#" className="text-gray-500 font-medium hover:text-black hover:bg-gray-100 p-2 rounded-lg duration-300">
+              Compras
+            </a>
+            <a href="#" className="text-gray-500 font-medium hover:text-black hover:bg-gray-100 p-2 rounded-lg duration-300">
+              Favoritos
+            </a>
+          </nav>
+          <div className="flex gap-3 items-center">
+            <button className="bg-rose-500 hover:bg-rose-600 duration-300 text-white flex items-center justify-center px-4 py-2 rounded font-medium">
+              <span className="hidden md:flex">Sign In</span>
+              <User2 />
+            </button>
+            <button id="btn-open" className="sm:hidden flex items-center justify-center px-4 py-2">
+              <Menu />
+            </button>
+          </div>
+        </header>
+        <Main />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,87 @@
+import { Map } from "lucide-react"
+
+const Main = () => {
+    return (
+        <main className="w-full flex flex-col pb-4">
+            <aside className="w-full px-3 lg:px-6 ">
+                <div className="flex w-full items-baseline justify-between">
+                    <h1 className="font-bold text-3xl mb-6 mt-4">Compre ou Alugue</h1>
+                    <span className="hidden md:block text-gray-600">Casas encontradas</span>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <a href="#" className="bg-white p-3 rounded-lg min-h-[400px] relative flex flex-col hover:shadow-lg duration-300">
+                        <div>
+                            <div className="group overflow-hidden rounded-lg">
+                                <div className="absolute z-40 top-5 left-5">
+                                    <div className="flex bg-slate-50/70 group-hover:bg-white duration-300 self-start items-center justify-center gap-2 px-3 py-1 rounded-full">
+                                        <Map />
+                                        <p className="font-medium text-sm">Fortaleza - CE</p> 
+                                    </div>
+                                </div>
+                                <img src="../assets/casa1.png" alt="Primeira casa" className="rounded-lg w-full h-64 object-cover group-hover:scale-110 duration-300" />                             </div>
+                            <div className="w-full flex flex-col gap-1 my-2">
+                                <h2 className="text-lg font-bold">Casa na praia</h2>
+                                <div>
+                                    <p className="text-sm font-bold text-indigo-500 my-1">R$ 2.200/mês</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-auto">
+                            <span className="font-medium text-sm">100 metros - </span>
+                            <span className="font-medium text-sm">4 quartos -</span>
+                            <span className="font-medium text-sm">3 banheiros</span>
+                        </div>
+                    </a>
+                    <a href="#" className="bg-white p-3 rounded-lg min-h-[400px] relative flex flex-col hover:shadow-lg duration-300">
+                        <div>
+                            <div className="group overflow-hidden rounded-lg">
+                                <div className="absolute z-40 top-5 left-5">
+                                    <div className="flex bg-slate-50/70 group-hover:bg-white duration-300 self-start items-center justify-center gap-2 px-3 py-1 rounded-full">
+                                        <Map />
+                                        <p className="font-medium text-sm">Fortaleza - CE</p> 
+                                    </div>
+                                </div>
+                                <img src="../assets/casa1.png" alt="Primeira casa" className="rounded-lg w-full h-64 object-cover group-hover:scale-110 duration-300" />                             </div>
+                            <div className="w-full flex flex-col gap-1 my-2">
+                                <h2 className="text-lg font-bold">Casa na praia</h2>
+                                <div>
+                                    <p className="text-sm font-bold text-indigo-500 my-1">R$ 2.200/mês</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-auto">
+                            <span className="font-medium text-sm">100 metros - </span>
+                            <span className="font-medium text-sm">4 quartos -</span>
+                            <span className="font-medium text-sm">3 banheiros</span>
+                        </div>
+                    </a>
+                    <a href="#" className="bg-white p-3 rounded-lg min-h-[400px] relative flex flex-col hover:shadow-lg duration-300">
+                        <div>
+                            <div className="group overflow-hidden rounded-lg">
+                                <div className="absolute z-40 top-5 left-5">
+                                    <div className="flex bg-slate-50/70 group-hover:bg-white duration-300 self-start items-center justify-center gap-2 px-3 py-1 rounded-full">
+                                        <Map />
+                                        <p className="font-medium text-sm">Fortaleza - CE</p> 
+                                    </div>
+                                </div>
+                                <img src="../assets/casa1.png" alt="Primeira casa" className="rounded-lg w-full h-64 object-cover group-hover:scale-110 duration-300" />                             </div>
+                            <div className="w-full flex flex-col gap-1 my-2">
+                                <h2 className="text-lg font-bold">Casa na praia</h2>
+                                <div>
+                                    <p className="text-sm font-bold text-indigo-500 my-1">R$ 2.200/mês</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-auto">
+                            <span className="font-medium text-sm">100 metros - </span>
+                            <span className="font-medium text-sm">4 quartos -</span>
+                            <span className="font-medium text-sm">3 banheiros</span>
+                        </div>
+                    </a>
+                </div>
+            </aside>
+        </main>
+    )
+}
+
+export default Main
