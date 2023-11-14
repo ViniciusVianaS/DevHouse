839013abc90b18eaf221679243932846db3b3c19
vinicius viana <viniciuspro333@gmail.com>
2023-11-14 11:44:35 -0300

    conectando o projeto a api feita com json-server

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,9 @@
 import { Map } from "lucide-react"
+import { useFetch } from "../hooks/useFetch"
 
 const Main = () => {
+    const url = "http://localhost:3000/products"
+    const { data: items, loading, error } = useFetch(url)
 
     return (
         <main className="w-full flex flex-col pb-4">
@@ -10,77 +13,35 @@ const Main = () => {
                     <span className="hidden md:block text-gray-600">Casas encontradas</span>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <a href="#" className="bg-white p-3 rounded-lg min-h-[400px] relative flex flex-col hover:shadow-lg duration-300">
-                        <div>
-                            <div className="group overflow-hidden rounded-lg">
-                                <div className="absolute z-40 top-5 left-5">
-                                    <div className="flex bg-slate-50/70 group-hover:bg-white duration-300 self-start items-center justify-center gap-2 px-3 py-1 rounded-full">
-                                        <Map />
-                                        <p className="font-medium text-sm">Fortaleza - CE</p> 
+                    {loading && <p>Carregando dados...</p>}
+                    {error && <p>{error}</p>}
+                    {items &&
+                        items.map((item) => (
+                            <a href="#" key={item.id} className="bg-white p-3 rounded-lg min-h-[400px] relative flex flex-col hover:shadow-lg duration-300">
+                                <div>
+                                    <div className="group overflow-hidden rounded-lg">
+                                        <div className="absolute z-40 top-5 left-5">
+                                            <div className="flex bg-slate-50/70 group-hover:bg-white duration-300 self-start items-center justify-center gap-2 px-3 py-1 rounded-full">
+                                                <Map />
+                                                <p className="font-medium text-sm">{item.cidade}</p>
+                                            </div>
+                                        </div>
+                                        <img className="rounded-lg w-full h-64 object-cover group-hover:scale-110 duration-300" src={process.env.PUBLIC_URL + '/' + item.imagem} alt={item.name} />
+                                    </div>
+                                    <div className="w-full flex flex-col gap-1 my-2">
+                                        <h2 className="text-lg font-bold">{item.name}</h2>
+                                        <div>
+                                            <p className="text-sm font-bold text-indigo-500 my-1">R$ {item.price}/mês</p>
+                                        </div>
                                     </div>
                                 </div>
-                                <img className="rounded-lg w-full h-64 object-cover group-hover:scale-110 duration-300" src="https://www.dicasdeviagem.com/wp-content/uploads/2021/02/casa-das-piscinas-airbnb-praia-do-forte.jpg" alt="primeira imagem" />                             
-                            </div>
-                            <div className="w-full flex flex-col gap-1 my-2">
-                                <h2 className="text-lg font-bold">Casa na praia</h2>
-                                <div>
-                                    <p className="text-sm font-bold text-indigo-500 my-1">R$ 2.200/mês</p>
+                                <div className="mt-auto">
+                                    <span className="font-medium text-sm">{item.tamanho} metros - </span>
+                                    <span className="font-medium text-sm">{item.quartos} quartos -</span>
+                                    <span className="font-medium text-sm">{item.banheiros} banheiros</span>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="mt-auto">
-                            <span className="font-medium text-sm">100 metros - </span>
-                            <span className="font-medium text-sm">4 quartos -</span>
-                            <span className="font-medium text-sm">3 banheiros</span>
-                        </div>
-                    </a>
-                    <a href="#" className="bg-white p-3 rounded-lg min-h-[400px] relative flex flex-col hover:shadow-lg duration-300">
-                        <div>
-                            <div className="group overflow-hidden rounded-lg">
-                                <div className="absolute z-40 top-5 left-5">
-                                    <div className="flex bg-slate-50/70 group-hover:bg-white duration-300 self-start items-center justify-center gap-2 px-3 py-1 rounded-full">
-                                        <Map />
-                                        <p className="font-medium text-sm">Fortaleza - CE</p> 
-                                    </div>
-                                </div>
-                                <img src="https://catracalivre.com.br/wp-content/uploads/2018/08/casas-mais-desejadas-airbnb-brasil.png" alt="Primeira casa" className="rounded-lg w-full h-64 object-cover group-hover:scale-110 duration-300" />                             
-                            </div>
-                            <div className="w-full flex flex-col gap-1 my-2">
-                                <h2 className="text-lg font-bold">Casa na praia</h2>
-                                <div>
-                                    <p className="text-sm font-bold text-indigo-500 my-1">R$ 2.200/mês</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="mt-auto">
-                            <span className="font-medium text-sm">100 metros - </span>
-                            <span className="font-medium text-sm">4 quartos -</span>
-                            <span className="font-medium text-sm">3 banheiros</span>
-                        </div>
-                    </a>
-                    <a href="#" className="bg-white p-3 rounded-lg min-h-[400px] relative flex flex-col hover:shadow-lg duration-300">
-                        <div>
-                            <div className="group overflow-hidden rounded-lg">
-                                <div className="absolute z-40 top-5 left-5">
-                                    <div className="flex bg-slate-50/70 group-hover:bg-white duration-300 self-start items-center justify-center gap-2 px-3 py-1 rounded-full">
-                                        <Map />
-                                        <p className="font-medium text-sm">Fortaleza - CE</p> 
-                                    </div>
-                                </div>
-                                <img src="https://viagemeturismo.abril.com.br/wp-content/uploads/2021/03/Praia-do-Silvera.jpg?quality=90&strip=info&w=720&h=440&crop=1" alt="Primeira casa" className="rounded-lg w-full h-64 object-cover group-hover:scale-110 duration-300" />                             </div>
-                            <div className="w-full flex flex-col gap-1 my-2">
-                                <h2 className="text-lg font-bold">Casa na praia</h2>
-                                <div>
-                                    <p className="text-sm font-bold text-indigo-500 my-1">R$ 2.200/mês</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="mt-auto">
-                            <span className="font-medium text-sm">100 metros - </span>
-                            <span className="font-medium text-sm">4 quartos -</span>
-                            <span className="font-medium text-sm">3 banheiros</span>
-                        </div>
-                    </a>
+                            </a>
+                        ))}
                 </div>
             </aside>
         </main>
